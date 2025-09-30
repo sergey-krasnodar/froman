202971fbc71b0ac4cd07f0e0cd0bc211d18f9469
sergey-krasnodar <serega.ru71@mail.ru>
2025-09-30 19:42:38 +0300

    added drop-down-menu

--- app/js/main.js
+++ app/js/main.js
@@ -2,13 +2,14 @@ const dropDownMenu = document.querySelector('.drop-down-menu');
 const btnBurgerMenu = document.querySelector('.header__burger-menu');
 const backdrop = document.querySelector('.backdrop');
 
-btnBurgerMenu.addEventListener('click', ()=>{
-  dropDownMenu.classList.toggle('drop-down-menu--active');
-  backdrop.classList.toggle('hidden');
+btnBurgerMenu.addEventListener('click', () => {
+  dropDownMenu.classList.add('drop-down-menu--active');
+  backdrop.classList.remove('hidden');
+  document.body.style.overflow = "hidden";
 })
 
-dropDownMenu.addEventListener('click', (e) => {
-  if (e.target === dropDownMenu) {
-    backdrop.classList.remove('hidden');
-  }
+backdrop.addEventListener('click', () => {
+  dropDownMenu.classList.remove('drop-down-menu--active');
+  backdrop.classList.add('hidden');
+  document.body.style.overflow = "";
 })
