878033c27a960bede21a737f46f2b3571e02ea72
sergey-krasnodar <serega.ru71@mail.ru>
2025-10-03 19:09:12 +0300

    fixed error in file main.js

--- app/js/main.js
+++ app/js/main.js
@@ -3,26 +3,24 @@ const btnBurgerMenu = document.querySelector('.header__burger-menu');
 const backdrop = document.querySelector('.backdrop');
 const btnCloseMenu = document.querySelector('.header__close-menu');
 
-btnBurgerMenu.addEventListener('click', () => {
+
+btnBurgerMenu.addEventListener('click', dropMenuOpen);
+backdrop.addEventListener('click', dropMenuClose);
+btnCloseMenu.addEventListener('click', dropMenuClose);
+btnBurgerMenu.addEventListener('click', dropMenuOpen);
+
+function dropMenuClose() {
+  dropDownMenu.classList.remove('drop-down-menu--active');
+  backdrop.classList.add('hidden');
+  btnBurgerMenu.classList.remove('hidden-menu');
+  btnCloseMenu.classList.add('hidden-menu');
+  document.body.style.overflow = "";
+}
+
+function dropMenuOpen() {
   dropDownMenu.classList.add('drop-down-menu--active');
   backdrop.classList.remove('hidden');
   document.body.style.overflow = "hidden";
   btnBurgerMenu.classList.add('hidden-menu');
   btnCloseMenu.classList.remove('hidden-menu');
-})
-
-backdrop.addEventListener('click', () => {
-  dropDownMenu.classList.remove('drop-down-menu--active');
-  backdrop.classList.add('hidden');
-  btnBurgerMenu.classList.remove('hidden-menu');
-  btnCloseMenu.classList.add('hidden-menu');
-  document.body.style.overflow = "";
-})
-
-btnCloseMenu.addEventListener('click', () => {
-  dropDownMenu.classList.remove('drop-down-menu--active');
-  backdrop.classList.add('hidden');
-  btnBurgerMenu.classList.remove('hidden-menu');
-  btnCloseMenu.classList.add('hidden-menu');
-  document.body.style.overflow = "";
-})
+}
